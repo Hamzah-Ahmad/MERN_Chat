79553c4c4cb504785f4711fc9b0360125b507f8c
Hamzah Ahmad <ahmadhamzah500@gmail.com>
2020-07-06 12:35:14 -0700

    Pushing to heroku

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,9 @@ if (process.env.NODE_ENV === "production") {
   //set static folder
   app.use(express.static("frontend/build"));
 
-  app.get("*", (req, res) => {});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 }
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
